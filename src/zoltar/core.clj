23da1c6e3808c2861b32d67f62bd5b3b43cf4524
Aaron Windsor <aaron.windsor@gmail.com>
2010-08-29 21:16:54 -0400

    initial commit with leiningen skeleton

---
+++ src/zoltar/core.clj
@@ -0,0 +1,1 @@
+(ns zoltar.core)
---
+++ test/zoltar/test/core.clj
@@ -0,0 +1,6 @@
+(ns zoltar.test.core
+  (:use [zoltar.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
