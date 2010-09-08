09400ef4724f335bfcfccbd742e160d6ac064321
Aaron Windsor <aaron.windsor@gmail.com>
2010-09-08 09:01:17 -0400

    basic working classifier, need to replace reduction by * to reduction by log+

--- src/zoltar/core.clj
+++ src/zoltar/core.clj
@@ -16,6 +16,54 @@
 		(max 1 (reduce + (vals dist))))))
   (add-point [this x] (assoc this :dist (inc-map dist x))))
 
+;tester is a map of the form:
+;  { :dist Distribution :testfunc TestFunction }
+(defn train-tester [tester sample]
+  ""
+  (assoc tester :dist
+    (add-point (tester :dist) ((tester :testfunc) sample))))
 
+(defn test-tester [tester sample]
+  ""
+  (prob (tester :dist) ((tester :testfunc) sample)))
 
+(defprotocol Model
+  ""
+  (train [this sample category] "")
+  (classify [this sample] "")
+  (compile-model [this] ""))
 
+(defn floored-distribution []
+  (FlooredDistribution. {} 0.01))
+
+(defn count-occur [string item]
+  (reduce + (map (fn [x] (if (= (str x) item) 1 0)) string)))
+
+(defn new-category []
+  [{:dist (floored-distribution)
+    :testfunc (fn [x] (count x)) }
+   {:dist (floored-distribution)
+    :testfunc (fn [x] (count-occur x " ")) }
+   {:dist (floored-distribution)
+    :testfunc (fn [x] (count-occur x "-")) }])
+
+(defn annotated-max [x y]
+  (if (> (last x) (last y)) x y))
+
+; category is a vector of testers
+; categories is a map from name -> category
+(defrecord NaiveBayesModel [categories]
+  Model
+  (train [this sample category]
+    (assoc this :categories
+      (assoc categories category
+        (vec (for [tester (get categories category (new-category))]
+	       (train-tester tester sample)))))) 
+  (classify [this sample]
+    (first
+      (reduce annotated-max
+        (for [[x y] (seq categories)]
+          [x (reduce * (map test-tester y (repeat sample)))]))))
+  (compile-model [this] this)) ;TODO: exclude irrelevant feature testers
+
+(defn naive-bayes-model [] (NaiveBayesModel. {}))
--- test/zoltar/test/core.clj
+++ test/zoltar/test/core.clj
@@ -7,3 +7,27 @@
   (testing
     (is (= (prob (FlooredDistribution. {} 0.123) "nothing") 0.123))
     (is (= (prob (FlooredDistribution. {:a 1} 0.01) :a) 1))))
+
+(deftest test-training
+  (testing
+    (is (= 1/3
+	   (-> { :dist (FlooredDistribution. {} 0.0) :testfunc count }
+	       (train-tester "aaa")
+	       (train-tester "abc")
+	       (train-tester "adss")
+	       (test-tester "1234"))))
+    (is (= 0.001
+	   (-> { :dist (FlooredDistribution. {} 0.001) :testfunc count }
+	       (train-tester "a")
+	       (train-tester "aa")
+	       (train-tester "aaa")
+	       (train-tester "aaaa")
+	       (test-tester "aaaaa"))))))
+
+(deftest test-classify
+  (testing
+      (is (= "long"
+	     (-> (naive-bayes-model)
+		 (train "a" "short")
+		 (train "fasdfsa" "long")
+		 (classify "abcdefg"))))))
